f8281336c5ba4935dca413623551491355a398ac
Bunnyfield <info@cybercraft.de>
2015-12-14 14:11:29 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description'      => 'TYPO3 compatible adaption of the \"Numo - Flat Portfolio for Creatives Theme\"',
 	'category'         => 'templates',
 	'shy'              => 0,
-	'version'          => '0.1.0',
+	'version'          => '7.0.0',
 	'dependencies'     => '',
 	'conflicts'        => '',
 	'priority'         => 'bottom',
@@ -33,9 +33,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.6.99',
-			'themes' => '1.0.1-2.99.99',
-			'theme_bootstrap' => '1.0.0-9.99.99',
+			'typo3' => '7.6.0-7.6.99',
+			'theme_bootstrap' => '7.0.0-7.99.99',
 		),
 		'conflicts' => array(
 		),
